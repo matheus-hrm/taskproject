c7542640ec4df19d397195bafa069dff0ca6144c
Matheus Henrique <rodriguesm673@gmail.com>
2023-08-15 16:36:59 -0300

    init

--- Tasklist_project/tasks/models.py
+++ Tasklist_project/tasks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
     
 
     def __str__(self):
